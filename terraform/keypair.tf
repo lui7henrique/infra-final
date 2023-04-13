7e7602c1b5b28cfeeeefd7ff97880db56959391a
Ector Cunha <ectorvccunha@outlook.com>
2023-04-12 21:41:25 -0300

    add comentarios aos arquivos keypair e main

--- terraform/keypair.tf
+++ terraform/keypair.tf
@@ -1,3 +1,5 @@
+// Aqui temos o par de chaves que o terraform usará para se autenticar na aws.
+// Pode usadas chaves locais ou as chaves diretamente da aws (AMI por exemplo).
 resource "aws_key_pair" "terraform-chave" {
   key_name   = "${var.username}-terraform-aws"
   public_key = file("~/.ssh/terraform-aws.pub")
